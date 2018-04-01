8e06fb386a993249b4b748a5efbe67fda7af62c5
JianangZhao <zhaojianang@hotmail.com>
2018-04-01 19:57:45 +0800

    Modify some bugs like when number  is 100001000, the output will be “壹亿零万壹千”

--- myJavaProject/src/com/adam/Num2RmbAdv2.java
+++ myJavaProject/src/com/adam/Num2RmbAdv2.java
@@ -15,10 +15,10 @@ public class Num2RmbAdv2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Num2RmbAdv2 nb = new Num2RmbAdv2();
-		double num = 1006.103;
+		double num = 100001000.173; //当第二节全为0时 会输出 “零万” 应该不输出
 		arr = nb.divide(num);
-		//System.out.println(Arrays.toString(nb.divide(num))); //输出 [1006, 33]
-		nb.toHanStr(num);
+//		System.out.println(Arrays.toString(nb.divide(num))); //输出 [1006, 33]
+		//nb.toHanStr(num);
 		System.out.println(nb.toHanStr(num));
 	}
 	/**
@@ -42,6 +42,7 @@ public class Num2RmbAdv2 {
 		// TODO Auto-generated method stub
 		//将一个整数转换为人民币读法字符串
 		String zheng = arr[0];
+		//System.out.println(zheng);
 		//求出整数部分的字符串长度 如 1006 长度位 4
 		int zhengLen = zheng.length();
 		int zhengYu = zhengLen % 4;
@@ -51,7 +52,8 @@ public class Num2RmbAdv2 {
 		String resultFirstZhengPart = "";
 		String resultSecondZhengPart = "";
 		String resultThirdZhengPart = "";
-		for(int i = 0; i < zhengPartLen; i ++){
+		String zhengResult = "";
+		for(int i = 0; i < zhengPartLen; i++){
 			if(i == 0){
 				//获取第一节数字字符串
 				String firstPart = zheng.substring(0, firstZhengPartLen);
@@ -71,12 +73,109 @@ public class Num2RmbAdv2 {
 							&& (firstPart.charAt(0) - 48) != 0
 							&& (firstPart.charAt(firstZhengPartLen-1) - 48) != 0){
 						resultFirstZhengPart += hanArr[number];
-					}
-					
+					}	
 				}
-//				System.out.println(resultFirstZhengPart);
+			//System.out.println(resultFirstZhengPart);
+			}
+			if(i == 1){
+				//获取第二节中的数字字符串
+				String secondPart = zheng.substring(firstZhengPartLen, (firstZhengPartLen+4));
+				for(int k = 0; k < secondPart.length(); k++){
+					int number = secondPart.charAt(k) - 48;
+//					System.out.print(number);
+					if( k != secondPart.length() - 1 && number != 0){
+						resultSecondZhengPart += hanArr[number] + unitArr[secondPart.length() - 2 -k];
+					}
+					if( k == secondPart.length() - 1 && number != 0){
+						resultSecondZhengPart += hanArr[number];
+					}
+					if(k == 2 && number == 0 && secondPart.charAt(0) - 48 == 0
+							&& secondPart.charAt(secondPart.length() - 1) - 48 != 0){
+						resultSecondZhengPart += hanArr[number];
+					}
+				}
+				//如果四位数字全是0, 且第二节不是最后一节, 则只需读出一个零。   
+                //Integer类的parseInt(String s)方法：将整数字符串参数解析成一个带符号的十进制整
+				if(Integer.parseInt(secondPart) == 0 && zhengPartLen == 3){
+					resultSecondZhengPart += hanArr[0];
+				}
+				//System.out.println(resultSecondZhengPart);
+			}
+			if(i == 2){
+				//获取第三节中的字符串
+				String thirdPart = zheng.substring(firstZhengPartLen + 4, zhengLen);
+				for(int l = 0; l < thirdPart.length(); l++){
+					int number = thirdPart.charAt(l) - 48;
+					if(l != thirdPart.length()-1 && number != 0){
+						resultThirdZhengPart += hanArr[number] + unitArr[thirdPart.length() - 2 -l];
+					}
+					if(l == thirdPart.length()-1 && number != 0){
+						resultThirdZhengPart += hanArr[number];
+					}
+					if(l == 2 && number == 0 && thirdPart.charAt(0) - 48 == 0
+							&& thirdPart.charAt(thirdPart.length()-1) - 48 != 0){
+						resultThirdZhengPart += hanArr[number];
+					}
+				}
+				//System.out.println(resultThirdZhengPart);
 			}
 		}
+			if(zhengPartLen == 1){
+				//如果第一节全为0
+				if(resultFirstZhengPart == ""){
+					zhengResult += hanArr[0] + "元";
+				}
+				else{
+				zhengResult += resultFirstZhengPart + "元";
+				}
+			}
+			if(zhengPartLen == 2){
+				if(Integer.parseInt(zheng.substring(firstZhengPartLen, firstZhengPartLen+4)) == 0){
+					zhengResult += resultFirstZhengPart + "万"; // bug point
+				}
+				else{
+					zhengResult += resultFirstZhengPart + "万" + resultSecondZhengPart + "元"; 
+				}
+			}
+			/*
+			 * 此处的逻辑如何处理才能应对所有的 senario
+			 */
+			if(zhengPartLen == 3){ 
+				//三节全为0
+				if(Integer.parseInt(zheng.substring(0, firstZhengPartLen)) == 0
+						&& Integer.parseInt(zheng.substring(firstZhengPartLen, firstZhengPartLen+4))== 0 
+						&& Integer.parseInt(zheng.substring(firstZhengPartLen + 4, zheng.length())) == 0){
+					zhengResult += "";
+				}
+				//前两节为0
+				else if(Integer.parseInt(zheng.substring(0, firstZhengPartLen)) == 0 
+						&& Integer.parseInt(zheng.substring(firstZhengPartLen, firstZhengPartLen+4)) == 0){
+					zhengResult += 	resultThirdZhengPart + "元";
+				}
+				//第一节 第三节为0
+				else if(Integer.parseInt(zheng.substring(0, firstZhengPartLen)) == 0
+						&& Integer.parseInt(zheng.substring(firstZhengPartLen + 4, zheng.length())) == 0){
+					zhengResult += resultSecondZhengPart +"万";
+				}
+				//后两节为0
+				else if(Integer.parseInt(zheng.substring(firstZhengPartLen, firstZhengPartLen+4))== 0 
+						&& Integer.parseInt(zheng.substring(firstZhengPartLen + 4, zheng.length())) == 0){
+					zhengResult += resultFirstZhengPart +"亿" ;
+				}
+				else if(Integer.parseInt(zheng.substring(0, firstZhengPartLen)) == 0 ){
+					zhengResult += resultFirstZhengPart + resultSecondZhengPart +"万"+ resultThirdZhengPart+ "元";
+				}
+				else if(Integer.parseInt(zheng.substring(firstZhengPartLen, firstZhengPartLen+4))== 0 ){
+					zhengResult += resultFirstZhengPart +"亿"+ resultSecondZhengPart + resultThirdZhengPart+ "元";
+				}
+				else if(Integer.parseInt(zheng.substring(firstZhengPartLen + 4, zheng.length())) == 0){
+					zhengResult += resultFirstZhengPart +"亿"+ resultSecondZhengPart +"万" + "元";
+				}
+				else{
+					zhengResult += resultFirstZhengPart +"亿"+ resultSecondZhengPart +"万"+ resultThirdZhengPart+ "元";
+				}
+			}
+		
 		//将一个小数部分转换为人民币读法字符串
 		String ling = arr[1];
 		int lingLen = ling.length();
@@ -98,7 +197,7 @@ public class Num2RmbAdv2 {
 		}
 //		System.out.println(resultLing);
 		//将整数和小数部分合并
-		return resultFirstZhengPart + resultLing;
+		return zhengResult + resultLing;
 
 	}
 
